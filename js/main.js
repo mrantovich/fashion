23ea5b7934178791ac728c5bde25a3594f5ebec2
Mikhail Rantovich <mrantovich@gmail.com>
2021-06-18 12:25:38 +0700

    Прокрутка меню на малых экранах

--- js/main.js
+++ js/main.js
@@ -2,12 +2,14 @@ const burger = document.querySelector('.burger');
 const header = document.querySelector('.header');
 const headerBox = document.querySelector('.header__box')
 const wholeBody = document.querySelector('body');
+const wholeHTML = document.querySelector('html');
 burger.addEventListener('click', toggleActive);
 
 function toggleActive() {
 	burger.classList.toggle('_active');
 	headerBox.classList.toggle('_active');
 	wholeBody.classList.toggle('_lock');
+
 };
 
 let headerHeight = header.offsetHeight;
@@ -19,6 +21,6 @@ function shadowOfHeader() {
 	else {
 		header.classList.remove('_shadowed');
 	}
-}
+};
 
 window.addEventListener('scroll', shadowOfHeader);
